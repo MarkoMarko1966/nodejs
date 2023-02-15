417eb47e4991f9b5623c9ab5aa51b9decf6d01ca
MarkoMarko1966 <markomickovski1966.mm@gmail.com>
2023-02-15 22:00:51 +0100

    Publish First Branch

--- c2/index.js
+++ c2/index.js
@@ -32,20 +32,87 @@
 // };
 
 // console.log(student);
-a = 10;
-b = 11;
-function testFn() {
-    console.log('a: ', a);
-    console.log('b: ', b);
-    return 'Test fn initiated';
+// a = 10;
+// b = 11;
+// function testFn() {
+//     console.log('a: ', a);
+//     console.log('b: ', b);
+//     return 'Test fn initiated';
+// }
+
+// console.log(testFn(a, b));
+// let test_fn = testFn(a, b);
+// console.log(test_fn);
+
+// const testFn1 = () => {
+//     console.log('test fn 1 initiated. This is an arrow function');
+// }
+
+// testFn1();
+
+// const testFn2 = (b, a = 25) => {
+//     return a + b;
+// };
+
+// console.log(testFn2(50))
+
+// const testFn3 = (o = {}) => {
+//     o.ime = "Zhivko";
+//     console.log(o);
+// }
+
+// const s = {ime:"Zlate", prezime: "Zlatevski"};
+// testFn3(s);
+// s.ime = "Zhivko";
+// console.log(s);
+
+// let uchenik = {ime: "Marko", ocena: 4};
+// let uchenik2 = uchenik;
+
+// console.log(uchenik);
+// uchenik.ime = "Vlado";
+// console.log(uchenik);
+// console.log(uchenik2);
+
+// const convert = (type, value) => {
+//     switch(type) {
+//         case 'c2f':
+//             return value * 9/5 + 32; 
+//         case 'f2c':
+//             return (value - 32) * 5/9;  
+//         default:
+//             console.log('cannot convert. The conversion type does not exist');
+//             return;
+//     }
+// }
+
+// let temp = 25;
+// console.log(convert('c2f', temp));
+// let res = convert('p2p', temp);
+// console.log(res);
+
+const student1 = {
+    ime: "John",
+    prezime: "Doe",
+    ocenka: 4
+};
+
+const student2 = {
+    ime: "Nikola",
+    prezime: "Redmond",
+    ocenka: 5
+};
+
+if (student1.ocenka > student2.ocenka) {
+    console.log(`${student1.ime} ${student1.prezime} e podobar od ${student2.ime} ${student2.prezime}.`)
 }
-
-console.log(testFn(a, b));
-let test_fn = testFn(a, b);
-console.log(test_fn);
-
-const testFn1 = () => {
-    console.log('test fn 1 initiated. This is an arrow function');
+else if (student1.ocenka < student2.ocenka){
+    console.log(`${student2.ime} ${student2.prezime} e podobar od ${student1.ime} ${student1.prezime}.`)
 }
+else {
+    console.log (`${student1.ime} ${student1.prezime} ima isto znaenje kako i ${student2.ime} ${student2.prezime}.`)
+};
 
-testFn1();
+
+
+
